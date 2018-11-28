61fd6af429469287f34741943a7a9228184e0c1e
yeongtze <yeongtze@googlemail.com>
2018-11-28 23:01:35 +0800

    Initial commit

--- SyntinelBot/EchoWithCounterBot.cs
+++ SyntinelBot/EchoWithCounterBot.cs
@@ -1,11 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace SyntinelBot
 {
@@ -24,6 +29,14 @@ namespace SyntinelBot
     {
         private readonly EchoBotAccessors _accessors;
         private readonly ILogger _logger;
+
+        private const string WelcomeText = @"Welcome to Syntinel channel. Syntinel Bot is at your service.";
+
+        // This array contains the file location of our adaptive cards
+        private readonly string[] _cards =
+        {
+            @".\Resources\Ec2ResizeCard.json"
+        };
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EchoWithCounterBot"/> class.
@@ -58,31 +71,112 @@ namespace SyntinelBot
         /// <seealso cref="IMiddleware"/>
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
+            if (turnContext == null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
+
             // Handle Message activity type, which is the main activity type for shown within a conversational interface
             // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
             // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
             if (turnContext.Activity.Type == ActivityTypes.Message)
             {
-                // Get the conversation state from the turn context.
-                var state = await _accessors.CounterState.GetAsync(turnContext, () => new CounterState());
+                //// Get the conversation state from the turn context.
+                //var state = await _accessors.CounterState.GetAsync(turnContext, () => new CounterState());
 
-                // Bump the turn count for this conversation.
-                state.TurnCount++;
+                //// Bump the turn count for this conversation.
+                //state.TurnCount++;
 
-                // Set the property using the accessor.
-                await _accessors.CounterState.SetAsync(turnContext, state);
+                //// Set the property using the accessor.
+                //await _accessors.CounterState.SetAsync(turnContext, state);
 
-                // Save the new turn count into the conversation state.
-                await _accessors.ConversationState.SaveChangesAsync(turnContext);
+                //// Save the new turn count into the conversation state.
+                //await _accessors.ConversationState.SaveChangesAsync(turnContext);
 
-                // Echo back to the user whatever they typed.
-                var responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'\n";
-                await turnContext.SendActivityAsync(responseMessage);
+                //// Echo back to the user whatever they typed.
+                //var responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'\n";
+                //await turnContext.SendActivityAsync(responseMessage);
+                if (!string.IsNullOrWhiteSpace(turnContext.Activity.Text) && turnContext.Activity.Text.ToLowerInvariant().Trim() == "notify")
+                {
+                    Random r = new Random();
+                    var cardAttachment = CreateAdaptiveCardAttachment(this._cards[r.Next(this._cards.Length)]);
+                    var reply = turnContext.Activity.CreateReply();
+                    reply.Attachments = new List<Attachment>() { cardAttachment };
+                    await turnContext.SendActivityAsync(reply, cancellationToken);
+                }
+                else if (turnContext.Activity.Value != null && turnContext.Activity.Value.GetType() == typeof(JObject))
+                {
+                    var responseValue = (JObject)turnContext.Activity.Value;
+                    var action = responseValue["action"].ToString();
+                    string responseMessage = string.Empty;
+                    switch (action)
+                    {
+                        case "resize":
+                            var instanceType = responseValue["instanceType"];
+                            // var vm = responseValue["msteams"]["value"];
+                            responseMessage = $"Action: {action}, New Instance Type: {instanceType}";
+                            break;
+                        case "ignore":
+                            responseMessage = $"Action: {action}";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    await turnContext.SendActivityAsync(responseMessage);
+                }
+            }
+            else if (turnContext.Activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (turnContext.Activity.MembersAdded != null)
+                {
+                    await SendWelcomeMessageAsync(turnContext, cancellationToken);
+                }
             }
             else
             {
-                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} activity detected", cancellationToken: cancellationToken);
             }
+        }
+
+        /// <summary>
+        /// Greet new users as they are added to the conversation.
+        /// </summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        /// <seealso cref="BotStateSet"/>
+        /// <seealso cref="ConversationState"/>
+        /// <seealso cref="IMiddleware"/>
+        private static async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in turnContext.Activity.MembersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(
+                        $"Welcome {member.Name}. {WelcomeText}",
+                        cancellationToken: cancellationToken);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates an <see cref="Attachment"/> that contains an <see cref="AdaptiveCard"/>.
+        /// </summary>
+        /// <param name="filePath">The path to the <see cref="AdaptiveCard"/> json file.</param>
+        /// <returns>An <see cref="Attachment"/> that contains an adaptive card.</returns>
+        private static Attachment CreateAdaptiveCardAttachment(string filePath)
+        {
+            var adaptiveCardJson = File.ReadAllText(filePath);
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
         }
     }
 }
